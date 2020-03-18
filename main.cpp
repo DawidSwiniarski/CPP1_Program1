b08d76d33b44e4eee256cd419ffea4563358d9b3
Dawid Swiniarski <dawid.sierodzok@gmail.com>
2020-03-18 21:39:54 +0100

    Inicjalizacja repozytorium

---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <fstream>
+#include <cstdlib>
+using namespace std;
+
+string imie, nazwisko;
+int nr_tel;
+
+
+int main() {
+    cout << "Zmiana hihi";
+    cout << "Podaj imie: ";
+    cin >> imie;
+    cout << "Podaj nazwisko: ";
+    cin >> nazwisko;
+    cout << "Podaj nr telefonu: ";
+    cin >> nr_tel;
+    cout << "Wprowadzona osoba: " << imie << "   " << nazwisko<< "   " << nr_tel;
+    fstream plik;
+    plik.open("wizyt owka.txt", ios::out | ios::app);
+    plik << imie << endl << nazwisko << endl << nr_tel;
+    plik.close();
+};
